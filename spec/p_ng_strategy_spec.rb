43749614fa50bcd70afe489ba187ccfdd228fbfa
Micah Shute <micah.shute@gmail.com>
2020-06-08 03:25:46 -0700

    add png_strategy_spec

--- spec/p_ng_strategy_spec.rb
+++ spec/p_ng_strategy_spec.rb
@@ -4,5 +4,17 @@ RSpec.describe Rbmach::PNGStrategy do
     expect(false).to eq(true)
   end
 
+  it 'correctly generates the PNG signature' do 
+    png = RBmach::PNGStrategy.new
+    png.bytes.take(8).to eq([137, 80, 78, 71, 13, 10, 26, 10])
+  end
+
+  it 'correclty generates the IHDR chunk' do 
+    expect(false).to eq(true)
+  end
+
+  
+
+
 end
 
